2d807b2676643299fbeb5e0743e67f2428743751
Trevor Reed <trevorar@usc.edu>
2013-03-23 00:57:37 -0700

    Implemented PuzzleMove class. close #4

--- puzzle_move.cpp
+++ puzzle_move.cpp
@@ -1,10 +1,28 @@
 #include "puzzle_move.h"
 
+PuzzleMove::PuzzleMove(Board &b) {
+	tileMove_ = 0;
+	b_ = &b;
+	g_ = 0;
+	h_ = 0;
+	prev_ = NULL;
+}
 
+PuzzleMove::PuzzleMove(int tile, Board *b, PuzzleMove *parent) {
+	tileMove_ = tile;
+	b_ = b;
+	g_ = parent->g_ + 1;
+	// h_ will be set later by PuzzleHeuristic object
+	h_ = 0
+	prev_ = parent;
+}
 
+PuzzleMove::~PuzzleMove() {
+	delete b_;
+	delete prev_;
+}
 
-
-// Compare to PuzzleMoves based on f distance (needed for priority queue
+// Compare to PuzzleMoves based on f distance (needed for priority queue)
 bool PuzzleMove::operator<(const PuzzleMove& p) const
 {
   if((g_ + h_) < (p.g_ + p.h_)){
@@ -30,4 +48,7 @@ bool PuzzleMove::operator>(const PuzzleMove& p) const
   }
   
 }
-
+bool PuzzleMove::operator==(const PuzzleMove& p) const
+{
+	return ((g_ + h_) == (p.g_ + p.h_) && (g_ == p.g_));
+}
