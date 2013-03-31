79a30f77b9912e3ffad5a67be72073f679ff8dab
Trevor Reed <trevorar@usc.edu>
2013-03-30 19:10:54 -0700

    Fixed all memory leaks! close #10

--- puzzle_solver.cpp
+++ puzzle_solver.cpp
@@ -24,42 +24,42 @@ int PuzzleSolver::run(PuzzleHeuristic *ph) {
 	PuzzleMove *potentialMove;
 	map<int, Board*> moves;
 	map<int, Board*>::iterator it;
-	
+
 	PuzzleMove *initialState = new PuzzleMove(b_);
 	initialState->h_ = ph->compute(initialState->b_->getTiles(), initialState->b_->getSize());
 	openList.push(initialState);
 	closedList.insert(initialState->b_);
-	
-	
+
+
 	while(!openList.empty()) {
-	
+
 		myMove = openList.top();
 		openList.pop();
 		garbageList.push_back(myMove);	
-			
+
 		if(myMove->b_->solved()) {
 			PuzzleMove *temp = myMove;
 			while(temp->prev_ != NULL) {
 				solution.push_back(temp->tileMove_);
 				temp = temp->prev_;
 			}
-			
+
 			cout << "Try this combination: ";
 			for(int i = solution.size() - 1; i >= 0; i--) {
 				cout << solution[i] << " ";
 			}
 			cout << endl;
-			
+
 			break;
 		}
-		
+
 		moves = myMove->b_->potentialMoves();
-		
+
 		for(it = moves.begin(); it != moves.end(); ++it) {
 			if(closedList.find(it->second) == closedList.end()) {
 				potentialMove = new PuzzleMove(it->first, it->second, myMove);
 				potentialMove->h_ = ph->compute(potentialMove->b_->getTiles(), potentialMove->b_->getSize());
-				
+
 				closedList.insert(potentialMove->b_);
 				openList.push(potentialMove);
 				expansions_++;
@@ -67,18 +67,23 @@ int PuzzleSolver::run(PuzzleHeuristic *ph) {
 			else {delete it->second;}
 		}
 	}
-	
+
 	while(!openList.empty()) {
 		delete openList.top();
 		openList.pop();
 	}
-	
 
-		
+	BoardSet::iterator it2;
+	for(it2 = closedList.begin(); it2 != closedList.end(); ++it2) {
+		if((*it2) != &b_) {
+			delete (*it2);
+		}
+	}
+
 	for(unsigned int i = 0; i < garbageList.size(); i++) {
 		delete garbageList[i];
 	}
-	
+
 	return 0;
 }
 
